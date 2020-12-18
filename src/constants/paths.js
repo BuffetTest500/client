51fa47c3fd4c1ba663d62bfe368a2b027245ee67
YOONA LEE <lya1290@gmail.com>
2020-12-19 00:37:28 +0900

    fix: change request server URI

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -2,7 +2,7 @@ const PATHS = {
   HOST: 'http://localhost',
   CLIENT_PORT: ':3000',
   SERVER_PORT: ':8080',
-  SERVER_URI: 'https://backend-dev2.ap-northeast-2.elasticbeanstalk.com',
+  SERVER_URI: 'http://backend-dev2.ap-northeast-2.elasticbeanstalk.com',
   ROOT: '/',
   SIGNUP: '/signup',
   LOGIN: '/login',
